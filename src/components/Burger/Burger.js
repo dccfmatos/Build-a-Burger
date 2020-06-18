bb2b6c2b65409c6b7619ab9fb1793efa4375b6ca
dccfmatos <d.catarina.c.f@gmail.com>
2020-06-18 01:06:16 +0100

    dynamic ingredient sum

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import style from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import { array } from 'prop-types';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
-        .map(igKey => {
-            return [...Array(props.ingredients[igKey])] //now we have an array of two elements [,]
-            .map((_, i) => {
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => { ////we'll have an array of two elements [,]
+            return [...Array(props.ingredients[igKey] )].map( (_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey}/>;
-            });
-        });
+            } );
+        } )
+        .reduce((arr, el) => {//allows to transform an array into something else 
+            return arr.concat(el)//it will return the updated arguments
+        }, []);//initial value of this array is empty
+        //with this the array is flatened and we can see the length of the array to know the sum
+        //of the ingredients
+
+        if (transformedIngredients.length === 0) {
+            transformedIngredients = <p>Please start adding ingredients!</p>;
+        }
 
     return (
         <div className= {style.Burger}>
